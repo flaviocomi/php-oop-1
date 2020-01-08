d05605f952e7cc7c58b5e32860e07a50d3b166a1
flaviocomi <flabio.co91@gmail.com>
2020-01-08 17:13:29 +0100

    complete

--- getAll.php
+++ getAll.php
@@ -3,28 +3,59 @@
 class Configurazione
 {
   // variabili
+  public $id;
+  public $title;
+  public $description;
 
-  function __construct(/* parametri*/)
+  function __construct($id, $title = '', $description = '')
   {
     // valorizzazione variabili tramite parametri
+    $this->id = $id;
+    $this->title = $title;
+    $this->description = $description;
   }
 
   //funzioni utili
 
   public function __toString()
   {
-
-    return /* rappresentazione testuale dell'oggetto */;
+    return "<p>[" . $this->id . "] "
+      . $this->title . " - "
+      . $this->description . '</p>';
   }
 }
 
 // connessione al DB
+// header('Content-Type: application/json');
+
+$server = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "hoteldb";
+$port = "3307";
+
+$conn = new mysqli($server, $username, $password, $dbname, $port);
+if ($conn->connect_errno) {
+
+  echo json_encode(-1);
+  return;
+}
 
 // download di tutte le configurazioni
+$sql = "
+  SELECT *
+  FROM configurazioni
+";
+
+$res = $conn->query($sql);
+if ($res->num_rows < 1) {
+
+  echo json_encode(-2);
+  return;
+}
 
 $confs = [];
 while ($conf = $res->fetch_assoc()) {
-
   $confs[] = new Configurazione(
     $conf['id'],
     $conf['title'],
@@ -32,7 +63,7 @@ while ($conf = $res->fetch_assoc()) {
   );
 }
 
-foreach ($conf as $confs) {
+foreach ($confs as $conf) {
 
   echo $conf;
 }
